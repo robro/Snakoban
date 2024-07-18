80b8f3f6e8c604ca5302a438c1a4b3237d70d72d
robro <433665+robro@users.noreply.github.com>
2024-07-18 00:07:08 -0700

    Add win and lose states

--- game.gd
+++ game.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-@export var grid: TileMap
+@onready var grid: TileMap = $Grid
+@onready var state_chart: StateChart = $StateChart
 
 var snake: Snake
+var snake_color: Color
 var box_points: Array[Vector2i]
 var tail_pos: Vector2i
+var flash_timer := Timer.new()
+var reset_timer := Timer.new()
 
 enum Layer {
-	DYNAMIC,
-	STATIC,
+	SNAKE,
+	SOLID,
+	PERMEABLE,
 }
 
 enum Id {
@@ -25,32 +30,36 @@ func _ready() -> void:
 
 	# Snake tiles are sorted by the order they were drawn in
 	# so draw them from head to tail
-	snake = Snake.new(grid.get_used_cells_by_id(Layer.DYNAMIC, Id.SNAKE))
+	snake = Snake.new(grid.get_used_cells(Layer.SNAKE))
 	snake.connect("want_move_to", _on_snake_want_move_to)
 	add_child(snake)
-	box_points.append_array(grid.get_used_cells_by_id(Layer.DYNAMIC, Id.BOX))
+	snake_color = grid.get_layer_modulate(Layer.SNAKE)
+	box_points.append_array(grid.get_used_cells_by_id(Layer.SOLID, Id.BOX))
+	$StateChart/Root/Lose.connect("state_entered", _on_lose_state_entered)
+	$StateChart/Root/Win.connect("state_entered", _on_win_state_entered)
 
 
 func _on_snake_want_move_to(point: Vector2i, direction: Vector2i) -> void:
-	var static_point_id := grid.get_cell_source_id(Layer.STATIC, point)
+	var world_point_id := grid.get_cell_source_id(Layer.SOLID, point)
 
-	if static_point_id == Id.WALLS:
+	if world_point_id == Id.WALLS:
 		return
 
-	if static_point_id == Id.FOOD:
-		grid.set_cell(Layer.STATIC, point)
+	if world_point_id == Id.FOOD:
+		grid.set_cell(Layer.SOLID, point)
 		snake.grow()
 	else:
 		for i in box_points.size():
 			if point == box_points[i]:
 				var box_point := point + direction
-				if (box_point in grid.get_used_cells(Layer.STATIC) or
-					box_point in snake.points or
-					box_point in box_points
+				if (box_point in grid.get_used_cells(Layer.SOLID) or
+					box_point in snake.points
 				):
 					return
 				else:
 					box_points[i] = box_point
+					grid.set_cell(Layer.SOLID, point)
+					grid.set_cell(Layer.SOLID, box_point, Id.BOX, Vector2i.ZERO)
 
 	snake.move_to(point)
 	queue_redraw()
@@ -62,26 +71,71 @@ func _input(event: InputEvent) -> void:
 
 
 func _draw() -> void:
-	grid.clear_layer(Layer.DYNAMIC)
+	grid.clear_layer(Layer.SNAKE)
+	grid.clear_layer(Layer.PERMEABLE)
 
 	for i in snake.points.size():
-		grid.set_cell(Layer.DYNAMIC, snake.points[i], Id.SNAKE, snake.get_tile(i))
+		grid.set_cell(Layer.SNAKE, snake.points[i], Id.SNAKE, snake.get_tile(i))
 
-	for i in box_points.size():
-		grid.set_cell(Layer.DYNAMIC, box_points[i], Id.BOX, Vector2i.ZERO)
-
-	for point in grid.get_used_cells_by_id(Layer.STATIC, Id.LASER):
+	for point in grid.get_used_cells_by_id(Layer.SOLID, Id.LASER):
 		var direction := Vector2.from_angle(
-			grid.get_cell_atlas_coords(Layer.STATIC, point).x * (PI / 2)
+			grid.get_cell_atlas_coords(Layer.SOLID, point).x * (PI / 2)
 		)
 		var beam_point := point + Vector2i(direction)
-		while (grid.get_cell_tile_data(Layer.STATIC, beam_point) == null):
-			if beam_point in box_points:
-				break
-			grid.set_cell(Layer.DYNAMIC, beam_point, Id.LASER, Vector2i(0, 1))
+		while (grid.get_cell_tile_data(Layer.SOLID, beam_point) == null):
+			grid.set_cell(Layer.PERMEABLE, beam_point, Id.LASER, Vector2i(0, 1))
 			beam_point += Vector2i(direction)
 
-	for death_point in grid.get_used_cells_by_id(Layer.DYNAMIC, Id.LASER):
+	for death_point in grid.get_used_cells_by_id(Layer.PERMEABLE, Id.LASER):
 		if death_point in snake.points:
-			get_tree().reload_current_scene()
+			state_chart.send_event("lost")
 			return
+
+	if grid.get_used_cells_by_id(Layer.SOLID, Id.FOOD).size() == 0:
+		state_chart.send_event("won")
+
+
+func _on_lose_state_entered() -> void:
+	flash_timer.wait_time = 0.1
+	flash_timer.autostart = true
+	flash_timer.connect("timeout", _on_lose_timer_timeout)
+	add_child(flash_timer)
+
+	reset_timer.wait_time = 1.0
+	reset_timer.autostart = true
+	reset_timer.connect("timeout", _on_reset_timer_timeout)
+	add_child(reset_timer)
+
+	snake.queue_free()
+
+
+func _on_lose_timer_timeout() -> void:
+	grid.set_layer_enabled(
+		Layer.SNAKE,
+		false if grid.is_layer_enabled(Layer.SNAKE) else true
+	)
+
+
+func _on_win_state_entered() -> void:
+	flash_timer.wait_time = 0.1
+	flash_timer.autostart = true
+	flash_timer.connect("timeout", _on_win_timer_timeout)
+	add_child(flash_timer)
+
+	reset_timer.wait_time = 1.0
+	reset_timer.autostart = true
+	reset_timer.connect("timeout", _on_reset_timer_timeout)
+	add_child(reset_timer)
+
+	snake.queue_free()
+
+
+func _on_win_timer_timeout() -> void:
+	grid.set_layer_modulate(
+		Layer.SNAKE,
+		snake_color if grid.get_layer_modulate(Layer.SNAKE) == Color.WHITE else Color.WHITE
+	)
+
+
+func _on_reset_timer_timeout() -> void:
+	get_tree().reload_current_scene()
