b4b2da2d887efb9347d58c3633297a4ea9bf4bcb
robro <433665+robro@users.noreply.github.com>
2024-07-27 21:48:57 -0700

    Remove stuff

--- global/levels.gd
+++ global/levels.gd
@@ -3,6 +3,4 @@ extends Node
 var curr_level_idx := 0
 const level_paths := [
     "res://scene/level_template.tscn"
-    # "res://scene/levels/level1.tscn",
-    # "res://scene/levels/level2.tscn",
 ]
