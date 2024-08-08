7003d21b414627d4a20f54e6da645577a535a161
robro <433665+robro@users.noreply.github.com>
2024-08-07 18:14:09 -0700

    Add mosaic effect for level transitions

--- scene/main.gd
+++ scene/main.gd
@@ -2,14 +2,26 @@ extends Node2D
 
 @export var reset_wait_time := 1.0
 var controlable := true
+var mosaic_timer := Timer.new()
+var mosaic_time := 0.25
 @onready var level : Node2D
 @onready var viewport : SubViewport = %"Game Viewport"
+@onready var mosaic : TextureRect = %Mosaic
 
 
 func _ready() -> void:
 	Events.win.connect(_on_events_win)
 	Events.lose.connect(_on_events_lose)
+	mosaic_timer.one_shot = true
+	add_child(mosaic_timer)
 	load_level()
+
+
+func _process(_delta: float) -> void:
+	if not mosaic_timer.is_stopped():
+		mosaic.material.set_shader_parameter(
+			"amount",
+			lerp(150, 0, mosaic_timer.time_left / mosaic_time))
 
 
 func load_level() -> void:
@@ -17,6 +29,7 @@ func load_level() -> void:
 		level.free()
 	level = load(Levels.level_paths[Levels.curr_level_idx]).instantiate()
 	viewport.add_child(level)
+	mosaic_timer.start(mosaic_time)
 
 
 func _on_events_win() -> void:
